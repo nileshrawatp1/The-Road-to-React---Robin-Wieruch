09618c77e1e28f9f34e08c6d36d61e0d83a02962
nileshrawatp1 <nileshrawatp1@gmail.com>
2023-03-05 13:58:05 +0530

    exercide pageNo: 46 done

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,16 +1,45 @@
 import * as React from 'react'
-function getTitle(title) {
-  return title;
-}
-const nums = [1, 2, 3, 4, 5];
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectId: 0
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectId: 1
+  }
+]
+
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
-      <input type="text" id="search" placeholder={nums.map(function (num) { return num * 2 })} />
+      <input type="text" id="search" placeholder="Text" />
+      <hr />
+      <h4>Used Map Method</h4>
+      <ul>
+        {list.map(function (item) {
+          return <li key={item.objectId}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        })}
+      </ul>
+      
     </div>
   )
 }
