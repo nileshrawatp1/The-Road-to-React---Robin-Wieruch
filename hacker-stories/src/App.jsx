58d6904d6c2852478f7a82a1dc82e388b0ba4b77
nileshrawatp1 <nileshrawatp1@gmail.com>
2023-03-13 08:35:25 +0530

    passed props to the list elemeant

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -2,14 +2,31 @@ import * as React from 'react'
 
 
 function App() {
-
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectId: 0
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectId: 1
+    }
+  ]
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
       <h4>Used Map Method</h4>
-      <List />
+      <List list={stories} />
     </div>
   )
 }
@@ -28,17 +45,15 @@ function Search() {
     <div>
       <label htmlFor="search">Search : </label>
       <input type="text" id="search" placeholder="Text" onChange={handleChange} />
-      <label htmlFor="blurEvent">On Blur : </label>
-      <input type="text" id="blur" placeholder="When clicked outside the element" onBlur={handleBlur} />
     </div>
   )
 }
 
-const List = () => (
+const List = (props) => (
   <div>
     <ul>
       {
-        list.map((item) => (
+        props.list.map((item) => (
           <li key={item.objectId}>
             <span>
               <a href={item.url}>{item.title}</a>
